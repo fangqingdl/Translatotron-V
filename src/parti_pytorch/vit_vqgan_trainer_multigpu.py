6d96cf83e51d2bfce4354d9c39092a408abb1ecd
jevinfang <jevinfang@tencent.com>
2025-08-23 19:32:55 +0800

    调试代码

--- src/parti_pytorch/vit_vqgan_trainer_multigpu.py
+++ src/parti_pytorch/vit_vqgan_trainer_multigpu.py
@@ -291,6 +291,7 @@ class VQGanVAETrainerMGPU(nn.Module):
                 self.accelerator.print(imgs_and_recons.max(), imgs_and_recons.min())
                 self.accelerator.print(imgs_and_recons.shape)
                 self.accelerator.print(imgs_and_recons)
+                imgs_and_recons = imgs_and_recons * 255
                 grid = make_grid(imgs_and_recons, nrow=2, padding=2, normalize=True)
 
                 logs['reconstructions'] = grid
