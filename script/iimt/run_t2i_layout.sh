8805758e55ea73261eda7287476aa2e7ba606cd4
jevinfang <jevinfang@tencent.com>
2025-08-28 10:48:42 +0800

    调试nan问题

--- script/iimt/run_t2i_layout.sh
+++ script/iimt/run_t2i_layout.sh
@@ -7,7 +7,7 @@ python -m torch.distributed.run --nproc_per_node=1 --master_port=27699 $prefix/s
     --valid_lmdb_path $prefix/data-build/iwslt14.de-en-lmdb/valid_ \
     --per_device_train_batch_size 10 \
     --per_device_eval_batch_size 10 \
-    --learning_rate 1e-4 \
+    --learning_rate 1.25e-5 \
     --num_train_epochs 100 \
     --gradient_accumulation_steps 1 \
     --num_warmup_steps 2000 \
@@ -20,7 +20,7 @@ python -m torch.distributed.run --nproc_per_node=1 --master_port=27699 $prefix/s
     --tgt_tokenizer_path $prefix/src/config/char_en.tokenizer \
     --vae_config_path $prefix/src/config/vit_vqgan_8192cb.json \
     --t2i_config_path $prefix/src/config/t2i_transformer_distill.json \
-    --vae_weight $prefix/image-tokenizer/en/vae.pt \
+    --vae_weight $prefix/image-tokenizer/en/vae.11000.pt \
     --resume_from_checkpoint $prefix/result_new/$save_name/epoch_6 \
     --use_amp true \
     --num_workers 16 \
